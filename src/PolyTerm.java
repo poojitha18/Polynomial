cb149200f7a34434a5131ce5e914d3628c17635d
poojitha18 <mnpoojitha@gmail.com>
2018-08-07 09:04:45 +0530

    Poly class added

---
+++ src/PolyTerm.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class PolyTerm {
+	int coefficient;
+	int power;
+	
+	PolyTerm(int coeff, int pow){
+		this.coefficient = coeff;
+		this.power = pow;
+	}
+}
--- src/Polynomial.java
+++ src/Polynomial.java
@@ -2,7 +2,11 @@ import java.util.*;
 import java.io.*;
 
 public class Polynomial {
-
+	
+	public static ArrayList<PolyTerm> addition(ArrayList<PolyTerm> poly1,ArrayList<PolyTerm> poly2){
+		ArrayList<PolyTerm> result = new ArrayList<PolyTerm>();
+		return result;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
